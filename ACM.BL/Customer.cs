4d7651d806222f719e69c357f000cf514dea57b6
vaviy <vaviy@VAVIYAS_11>
2022-01-21 08:27:20 -0500

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string Firstname { get; set; }
+        public string Fullname
+        {
+            get
+            {
+                return Lastname + "," + Firstname;
+            }
+        }
+
+        private string _lastname; public string Lastname
+        {
+            get
+            {
+                return _lastname;
+            }
+            set
+            {
+                _lastname = value;
+            }
+        }
+    }
+}
