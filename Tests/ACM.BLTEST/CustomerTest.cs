abd93607b6360952d2452e826a3943251df5ccfb
Hardik_5894 <Vaviya.365@gmail.com>
2022-01-21 13:30:08 -0500

    Code with Unit Test

---
+++ Tests/ACM.BLTEST/CustomerTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ACM.BL;
+
+namespace ACM.BLTEST
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // - Arrange
+            /* var customer is same
+               var customer = new Customer();  */
+
+            Customer customer = new Customer
+            {
+
+                Firstname = "Bildo",
+                Lastname = "Baggins"
+
+            };
+
+            string expected = "Baggins,Bildo";
+
+            // - Act
+
+            string actual = customer.Fullname;
+
+            // - Assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void FullNameEmpty()
+        {
+            // - Arrange
+            Customer customer = new Customer
+            {
+
+                Firstname = "Bildo",
+                Lastname = "Baggins"
+
+            };
+
+            // - Act
+
+
+
+            // - Assert
+
+
+        }
+    }
+}
---
+++ Tests/ACM.BLTEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTEST")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b4719b9a-9964-4d9b-9845-9bae0af66ac9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
